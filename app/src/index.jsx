cb99d730875df3776ad8da2e563f7aea5e6ba275
Florent Sorel <florent.sorel@gmail.com>
2017-02-27 20:34:05 +0100

    fix: corrige l'import du composant contenant l'application

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'redux'
-import Root from 'components/Root'
+import App from 'components/App'
 import configureStore from './store'
 
 const store = configureStore()
 
 ReactDOM.render(
-  <Root store={store} />,
+  <App store={store} />,
   document.getElementById('app')
 )
